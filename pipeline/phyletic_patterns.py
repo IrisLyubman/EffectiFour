ba549f7715b9c9b61fca18d114060fe2951ecdb1
Manage Effectidor Website <effectidor@effectidor.tau.ac.il>
2026-05-03 15:22:20 +0300

    Fix bug in phyletic_patterns.py

--- pipeline/phyletic_patterns.py
+++ pipeline/phyletic_patterns.py
@@ -5,60 +5,113 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from sys import argv
+import re
 
 working_directory = argv[1]
 
-
-def create_phyletic_patterns(in_file, phyletic_csv, phyletic_text, gene_order, start_col=4, delimiter=',',
-                             T4Es=False):
-    '''
-    in_file: path to csv file with the data to transform to phyletic pattern, where the columns are genomes, and the
-    rows are genes/OGs
-    phyletic_csv: path to a csv file that will be created with the phyletic pattern. it will be used later for the
-    presence/absense map
-    phyletic_text: a path to the output phyletic pattern  that will be created, in txt format
-    gene_order: a path to an output txt file that will hold a list of the gene order in the phyletic pattern
-    start_col: position of the first column in the input file that represents a genome
-    delimiter: delimiter to use to read the dataframe (, for csv and \t for tsv)
-    T4Es: whether the input is T4Es prediction (this input requires dropping columns that do not exist elsewhere)
-    '''
+def create_phyletic_patterns(in_file, phyletic_csv, phyletic_text, gene_order, start_col=4, delimiter=',', T4Es=False):
     df = pd.read_csv(in_file, sep=delimiter)
     header = df.columns
     species = header[start_col:]
+
     df.set_index(header[0], inplace=True)
     df.dropna(how='all', subset=species, inplace=True)
     df.fillna('0', inplace=True)
-    df[species] = np.where(df[species] != '0', '1', '0')  # put 1 wherever there's a locus
+    df[species] = np.where(df[species] != '0', '1', '0')
+
     if T4Es:
         predicted = df[(df['is_effector'] == '?') & (df['score'] > 0.5)][species]
         positives = df[df.is_effector == 'yes'][species]
         positives, predicted = positives.T, predicted.T
         phyletic_df = positives.merge(predicted, left_index=True, right_index=True)
-    else:  # T4SS
+    else:
         phyletic_df = df.T
+
     phyletic_df.to_csv(phyletic_csv)
+
     phyletic_sequence = phyletic_df.apply(lambda row: ''.join(row.values.astype(str)), axis=1)
+
     with open(phyletic_text, 'w') as out_phyletic:
         for i in range(len(phyletic_sequence)):
             out_phyletic.write(f'>{phyletic_sequence.index[i]}\n')
             out_phyletic.write(f'{phyletic_sequence.iloc[i]}\n')
+
     with open(gene_order, 'w') as out_gene_order:
         out_gene_order.write(','.join(phyletic_df.columns))
 
+def simplify_T4SS_component(annotation):
+    if pd.isna(annotation):
+        return annotation
 
-# generating clustered presence/absence map
-# working_directory = r'C:\Users\TalPNB2\Downloads'
+    annotation = str(annotation)
 
+    component_match = re.search(
+        r'((?:Icm[A-Za-z0-9]+|Dot[A-Za-z0-9]+|LvgA)(?:/(?:Icm[A-Za-z0-9]+|Dot[A-Za-z0-9]+|LvgA))*)',
+        annotation
+    )
 
-def create_presence_absence_map(phyletic_csv, x_label, out_fig_path, colors=['silver', 'lightseagreen'],
-                                base_font_size=8, col_cluster=True):
+    if component_match:
+        return component_match.group(1)
+
+    return annotation
+
+def create_T4SS_phyletic_patterns(in_file, phyletic_csv, phyletic_text, gene_order):
+    df = pd.read_csv(in_file)
+
+    df = df.dropna(subset=['Bacterial Protein ID'])
+    df = df[df['Bacterial Protein ID'].astype(str).str.strip() != '']
+
+    df['T4SS component'] = df['T4SS Hit Annotation'].apply(simplify_T4SS_component)
+    df['present'] = 1
+
+    phyletic_df = df.pivot_table(
+        index='Genome',
+        columns='T4SS component',
+        values='present',
+        aggfunc='max',
+        fill_value=0
+    )
+
+    phyletic_df.to_csv(phyletic_csv)
+
+    phyletic_sequence = phyletic_df.apply(lambda row: ''.join(row.values.astype(str)), axis=1)
+
+    with open(phyletic_text, 'w') as out_phyletic:
+        for i in range(len(phyletic_sequence)):
+            out_phyletic.write(f'>{phyletic_sequence.index[i]}\n')
+            out_phyletic.write(f'{phyletic_sequence.iloc[i]}\n')
+
+    with open(gene_order, 'w') as out_gene_order:
+        out_gene_order.write(','.join(phyletic_df.columns))
+
+def create_presence_absence_map(
+        phyletic_csv,
+        x_label,
+        out_fig_path,
+        colors=['silver', 'lightseagreen'],
+        base_font_size=9,
+        col_cluster=True,
+        width_per_col=0.08,
+        height_per_row=0.35,
+        min_width=12,
+        max_width=30,
+        min_height=6,
+        max_height=16):
+
     df = pd.read_csv(phyletic_csv)
     df.set_index(df.columns[0], inplace=True)
+
     n_rows, n_cols = df.shape
-    fig_width = max(n_cols * 0.18, 14)
-    fig_height = max(n_rows * 0.22, 4)
+    fig_width = max(n_cols * width_per_col, min_width)
+    fig_height = max(n_rows * height_per_row, min_height)
+
+    if max_width is not None:
+        fig_width = min(fig_width, max_width)
+    if max_height is not None:
+        fig_height = min(fig_height, max_height)
 
     cmap = LinearSegmentedColormap.from_list('Custom', colors, len(colors))
+
     clustermap = sns.clustermap(
         df,
         cmap=cmap,
@@ -68,70 +121,118 @@ def create_presence_absence_map(phyletic_csv, x_label, out_fig_path, colors=['si
         row_cluster=False,
         vmin=0,
         vmax=1,
-        cbar_pos=(0.02, 0.75, 0.03, 0.15)
+        cbar_pos=(0.03, 0.75, 0.025, 0.12)
     )
+
     clustermap.ax_heatmap.xaxis.set_label_position('top')
     clustermap.ax_heatmap.xaxis.tick_top()
-    if n_cols > 120:
-        clustermap.ax_heatmap.set_xticklabels([])
-    else:
-        plt.setp(
-            clustermap.ax_heatmap.xaxis.get_majorticklabels(),
-            rotation=60,
-            ha='left',
-            fontsize=base_font_size
-        )
+
+    plt.setp(
+        clustermap.ax_heatmap.xaxis.get_majorticklabels(),
+        rotation=60,
+        ha='left',
+        fontsize=base_font_size
+    )
+
     plt.setp(
         clustermap.ax_heatmap.yaxis.get_majorticklabels(),
         fontsize=base_font_size
     )
-    clustermap.ax_heatmap.set_xlabel(x_label, fontsize=base_font_size + 2, labelpad=10)
+
+    clustermap.ax_heatmap.set_xlabel(x_label, fontsize=base_font_size + 2, labelpad=12)
     clustermap.ax_heatmap.set_ylabel('Genome', fontsize=base_font_size + 2, labelpad=10)
+
     colorbar = clustermap.ax_heatmap.collections[0].colorbar
     colorbar.set_ticks([0.25, 0.75])
     colorbar.set_ticklabels(['Absent', 'Present'])
     colorbar.ax.tick_params(labelsize=base_font_size)
+
     clustermap.fig.savefig(
         out_fig_path,
-        dpi=300,
+        dpi=180,
         bbox_inches='tight',
-        pad_inches=0.1
+        pad_inches=0.15
     )
 
     plt.close(clustermap.fig)
 
+# T4Es
 
-# T4Es:
+in_f_T4Es = os.path.join(
+    working_directory,
+    'out_learning',
+    'consensus_predictions_with_annotations_and_ortho_table.csv'
+)
 
-in_f_T4Es = os.path.join(working_directory, 'out_learning',
-                         'consensus_predictions_with_annotations_and_ortho_table.csv')
 phyletic_csv_T4Es = os.path.join(working_directory, 'PresenceAbsence_T4Es.csv')
 phyletic_text_T4Es = os.path.join(working_directory, 'Effectors_phyletic_pattern.txt')
 gene_order_T4Es = os.path.join(working_directory, 'T4Es_order_in_PhyleticPattern.txt')
-create_phyletic_patterns(in_f_T4Es, phyletic_csv_T4Es, phyletic_text_T4Es, gene_order_T4Es, T4Es=True)
-create_presence_absence_map(phyletic_csv_T4Es, 'T4E (ortholog group)', os.path.join(working_directory,
-                                                                                    'T4Es_presence_absence.png'))
 
-# T4SS:
+create_phyletic_patterns(
+    in_f_T4Es,
+    phyletic_csv_T4Es,
+    phyletic_text_T4Es,
+    gene_order_T4Es,
+    T4Es=True
+)
+
+create_presence_absence_map(
+    phyletic_csv_T4Es,
+    'T4E (ortholog group)',
+    os.path.join(working_directory, 'T4Es_presence_absence.png'),
+    base_font_size=8,
+    width_per_col=0.12,
+    height_per_row=0.55,
+    min_width=18,
+    max_width=45,
+    min_height=10,
+    max_height=28
+)
+
+# T4SS
 
 in_file_T4SS = os.path.join(working_directory, 'T4SS.csv')
 phyletic_csv_T4SS = os.path.join(working_directory, 'PresenceAbsence_T4SS.csv')
 phyletic_text_T4SS = os.path.join(working_directory, 'T4SS_phyletic_pattern.txt')
 gene_order_T4SS = os.path.join(working_directory, 'T4SS_order_in_PhyleticPattern.txt')
-create_phyletic_patterns(in_file_T4SS, phyletic_csv_T4SS, phyletic_text_T4SS, gene_order_T4SS, start_col=1)
-create_presence_absence_map(phyletic_csv_T4SS, 'T4SS component', os.path.join(working_directory,
-                                                                              'T4SS_presence_absence.png'),
-                            col_cluster=False)
 
-# chaperones:
+create_T4SS_phyletic_patterns(
+    in_file_T4SS,
+    phyletic_csv_T4SS,
+    phyletic_text_T4SS,
+    gene_order_T4SS
+)
+
+create_presence_absence_map(
+    phyletic_csv_T4SS,
+    'T4SS component',
+    os.path.join(working_directory, 'T4SS_presence_absence.png'),
+    col_cluster=False
+)
+
+# chaperones
+
 in_f_chaperones = os.path.join(working_directory, 'chaperones.csv')
 
 if os.path.exists(in_f_chaperones):
     df = pd.read_csv(in_f_chaperones)
     n_rows, n_cols = df.shape
+
     if n_rows > 1 and n_cols > 1:
         phyletic_csv_chaperones = os.path.join(working_directory, 'PresenceAbsence_chaperones.csv')
         phyletic_text_chaperones = os.path.join(working_directory, 'chaperones_phyletic_pattern.txt')
         gene_order_chaperones = os.path.join(working_directory, 'chaperones_order_in_PhyleticPattern.txt')
-        create_phyletic_patterns(in_f_chaperones, phyletic_csv_chaperones, phyletic_text_chaperones, gene_order_chaperones, start_col=1)
-        create_presence_absence_map(phyletic_csv_chaperones, 'chaperone', os.path.join(working_directory, 'chaperones_presence_absence.png'))
+
+        create_phyletic_patterns(
+            in_f_chaperones,
+            phyletic_csv_chaperones,
+            phyletic_text_chaperones,
+            gene_order_chaperones,
+            start_col=1
+        )
+
+        create_presence_absence_map(
+            phyletic_csv_chaperones,
+            'chaperone',
+            os.path.join(working_directory, 'chaperones_presence_absence.png')
+        )
